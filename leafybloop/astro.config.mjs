1c14240c6c318c4b82953dc04d9e3b59b3993f1a
Zee Aslam <zeet6613@gmail.com>
2022-02-05 10:34:02 -0500

    Add renderers

--- leafybloop/astro.config.mjs
+++ leafybloop/astro.config.mjs
@@ -11,5 +11,11 @@ export default {
     // hostname: 'localhost',  // The hostname to run the dev server on.
     // port: 3000,             // The port to run the dev server on.
   },
-  renderers: [],
+  renderers: [
+    "@astrojs/renderer-preact",
+    "@astrojs/renderer-react",
+    "@astrojs/renderer-solid",
+    "@astrojs/renderer-svelte",
+    "@astrojs/renderer-vue"
+  ],
 };
