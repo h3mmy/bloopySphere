4f491a9169d4ed896fa71bb85e49b43239036654
Zee Aslam <zeet6613@gmail.com>
2022-02-15 19:34:51 -0500

    Enable ddns

---
+++ fix-crd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+KIND=Installation
+NAME=default
+RELEASE=tigera-operator
+NAMESPACE=tigera-operator
+echo "Using following values..."
+echo Kind: $KIND
+echo NAME: $NAME
+echo RELEASE: $RELEASE
+echo NAMESPACE: $NAMESPACE
+kubectl -n $NAMESPACE annotate $KIND $NAME meta.helm.sh/release-name=$RELEASE
+kubectl -n $NAMESPACE annotate $KIND $NAME meta.helm.sh/release-namespace=$NAMESPACE
+kubectl -n $NAMESPACE label $KIND $NAME app.kubernetes.io/managed-by=Helm
