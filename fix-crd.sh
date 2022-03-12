6d3d5e8af19b5a56a41d60ec0271971633b6b07b
Zee Aslam <zeet6613@gmail.com>
2022-03-11 23:24:34 -0500

    Changes for rook-ceph

--- fix-crd.sh
+++ fix-crd.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
-KIND=Installation
-NAME=default
-RELEASE=cert-manager
-NAMESPACE=cert-manager
-echo "Using following values..."
-echo Kind: $KIND
-echo NAME: $NAME
-echo RELEASE: $RELEASE
-echo NAMESPACE: $NAMESPACE
-kubectl -n $NAMESPACE annotate $KIND $NAME meta.helm.sh/release-name=$RELEASE
-kubectl -n $NAMESPACE annotate $KIND $NAME meta.helm.sh/release-namespace=$NAMESPACE
-kubectl -n $NAMESPACE label $KIND $NAME app.kubernetes.io/managed-by=Helm
+KIND=CustomResourceDefinition
+RELEASE=rook-ceph
+NAMESPACE=rook-ceph
+
+for CRD in objectbuckets.objectbucket.io volumes.rook.io objectbucketclaims.objectbucket.io cephobjectzones.ceph.rook.io cephrbdmirrors.ceph.rook.io cephobjectzonegroups.ceph.rook.io cephobjectstoreusers.ceph.rook.io cephblockpools.ceph.rook.io cephobjectstores.ceph.rook.io cephobjectrealms.ceph.rook.io cephnfses.ceph.rook.io cephclients.ceph.rook.io cephfilesystemsubvolumegroups.ceph.rook.io cephfilesystemmirrors.ceph.rook.io cephclusters.ceph.rook.io cephbucketnotifications.ceph.rook.io cephbuckettopics.ceph.rook.io cephfilesystems.ceph.rook.io; do
+  # echo "Using following values..." \
+  # echo Kind: $KIND \
+  # echo NAME: $CRD \
+  # echo RELEASE: $RELEASE \
+  # echo NAMESPACE: $NAMESPACE \
+  kubectl -n ${NAMESPACE} annotate ${KIND} ${CRD} meta.helm.sh/release-name=${RELEASE};
+  kubectl -n ${NAMESPACE} annotate ${KIND} ${CRD} meta.helm.sh/release-namespace=${NAMESPACE};
+  kubectl -n ${NAMESPACE} label $KIND ${CRD} app.kubernetes.io/managed-by=Helm;
+done
+# kubectl annotate $KIND $CRD meta.helm.sh/release-name=$RELEASE
+# kubectl annotate $KIND $CRD meta.helm.sh/release-namespace=$NAMESPACE
+# kubectl label $KIND $CRD app.kubernetes.io/managed-by=Helm
+# cephblockpools.ceph.rook.io
+# cephbucketnotifications.ceph.rook.io
+# cephbuckettopics.ceph.rook.io
+# cephclients.ceph.rook.io
+# cephclusters.ceph.rook.io
+# cephfilesystemmirrors.ceph.rook.io
+# cephfilesystemsubvolumegroups.ceph.rook.io
+# cephfilesystems.ceph.rook.io
+# cephobjectstores.ceph.rook.io
+# cephobjectrealms.ceph.rook.io
+# cephobjectstoreusers.ceph.rook.io
+# cephobjectzonegroups.ceph.rook.io
+# cephobjectzones.ceph.rook.io
+# cephrbdmirrors.ceph.rook.io
+# objectbucketclaims.objectbucket.io
+# objectbuckets.objectbucket.io
