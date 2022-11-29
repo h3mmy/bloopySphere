949ef3962189be3b88113e4abf1277de2924968a
Zee Aslam <zeet6613@gmail.com>
2022-11-29 18:47:42 -0500

    (authentik) Refactor Kubegres arrangement

--- cluster/apps/auth/authentik/postgres/primary_init_script.sh
+++ cluster/apps/auth/authentik/postgres/primary_init_script.sh
@@ -4,16 +4,18 @@ set -e
 dt=$(date '+%d/%m/%Y %H:%M:%S');
 echo "$dt - Running init script the 1st time Primary PostgreSql container is created...";
 
-customDatabaseName="$EMQX_DB_NAME"
-customUserName="$EMQX_DB_USER"
+customDatabaseName="authentik"
+customUserName="authentik"
 
 echo "$dt - Running: psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER --dbname $POSTGRES_DB ...";
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 CREATE DATABASE $customDatabaseName;
-CREATE USER $customUserName WITH PASSWORD '$EMQX_DB_PASSWORD';
+CREATE USER $customUserName WITH PASSWORD '$SECRET_AUTHENTIK_POSTGRES_PASSWORD';
 GRANT ALL PRIVILEGES ON DATABASE $customDatabaseName to $customUserName;
 ALTER DATABASE $customDatabaseName OWNER TO $customUserName;
+# Required for postgres 15+ pending authentik upgrade
+ALTER USER $customUserName WITH SUPERUSER;
 EOSQL
 
 echo "$dt - Init script is completed";
