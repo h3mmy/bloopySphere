f0aa9b2dbf0045b44f246f6ff3f9e536a60a156a
Zee Aslam <zeet6613@gmail.com>
2022-12-07 11:10:01 -0500

    Update locals.tf
    Signed-off-by: Zee Aslam <zeet6613@gmail.com>

--- infrastructure/terraform/cloudflare/locals.tf
+++ infrastructure/terraform/cloudflare/locals.tf
@@ -1,6 +1,6 @@
 locals {
   cloudflare_secrets = sensitive(yamldecode(nonsensitive(data.sops_file.cloudflare_secrets.raw)))
-  domains            = yamldecode(nonsensitive(data.sops_file.domains.raw))
+  domains            = yamldecode(nonsensitive(data.sops_external.domains.raw))
   status_dev_ipv4    = "192.0.2.1"
   status_dev_ipv6    = "100::"
   gitlab_repo = "h3mmy.gitlab.io"
