70e11e40d74a31fbe94c946c0b0d1c1005874b0f
bloopy-boi <166688+bloopy-boi[bot]@users.noreply.github.com>
2023-01-25 02:42:59 +0000

    fix(deps): update terraform cloudflare to 3.33.1
    Signed-off-by: Zee Aslam <h3mmy@users.noreply.github.com>

--- infrastructure/terraform/cloudflare/versions.tf
+++ infrastructure/terraform/cloudflare/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "3.33.0"
+      version = "3.33.1"
     }
     sops = {
       source  = "carlpett/sops"
