4071e1838e6ba4bd68b13fd4a30fa6f0deb1531d
Zee Aslam <zeet6613@gmail.com>
2023-01-25 19:01:27 -0500

    (blocky) tweaks

--- fix-crd.sh
+++ fix-crd.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 KIND=CustomResourceDefinition
-RELEASE=ghost-mysql
-NAMESPACE=blogs
+RELEASE=cert-manager
+NAMESPACE=cert-manager
 
 # for CRD in objectbuckets.objectbucket.io volumes.rook.io objectbucketclaims.objectbucket.io cephobjectzones.ceph.rook.io cephrbdmirrors.ceph.rook.io cephobjectzonegroups.ceph.rook.io cephobjectstoreusers.ceph.rook.io cephblockpools.ceph.rook.io cephobjectstores.ceph.rook.io cephobjectrealms.ceph.rook.io cephnfses.ceph.rook.io cephclients.ceph.rook.io cephfilesystemsubvolumegroups.ceph.rook.io cephfilesystemmirrors.ceph.rook.io cephclusters.ceph.rook.io cephbucketnotifications.ceph.rook.io cephbuckettopics.ceph.rook.io cephfilesystems.ceph.rook.io; do
-for CRD in innodbclusters.mysql.oracle.com; do
+for CRD in issuers.cert-manager.io orders.acme.cert-manager.io clusterissuers.cert-manager.io challenges.acme.cert-manager.io certificates.cert-manager.io certificaterequests.cert-manager.io; do
   # echo "Using following values..." \
   # echo Kind: $KIND \
   # echo NAME: $CRD \
