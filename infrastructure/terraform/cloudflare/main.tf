36d0b5fbdee892b97e9159fe56063850d1195cfe
Zee Aslam <zeet6613@gmail.com>
2023-02-19 10:21:40 -0500

    (tf-cloudflare) move some stuff around

--- infrastructure/terraform/cloudflare/main.tf
+++ infrastructure/terraform/cloudflare/main.tf
@@ -4,13 +4,8 @@ data "sops_file" "cloudflare_secrets" {
   source_file = "secrets.sops.yaml"
 }
 
-data "sops_external" "domains" {
-  source = data.http.bloopysphere_domains.response_body
-  input_type = "yaml"
-}
-
-data "http" "bloopysphere_domains" {
-  url = "https://raw.githubusercontent.com/h3mmy/bloopysphere/main/infrastructure/shared/domains.sops.yaml"
+data "sops_file" "domains" {
+  source_file = "domains.sops.yaml"
 }
 
 # Obtain current home IPv4 address
