e508d72f15371e8c97d1c2acc1944991f728f309
Zee Aslam <zeet6613@gmail.com>
2023-02-25 16:38:34 -0500

    finalize functional action

--- infrastructure/terraform/cloudflare/main.tf
+++ infrastructure/terraform/cloudflare/main.tf
@@ -10,8 +10,3 @@ data "sops_file" "domains" {
 data "http" "ipv4_lookup_raw" {
   url = "http://ipv4.icanhazip.com"
 }
-
-# Obtain current home IPv6 address
-# data "http" "ipv6_lookup_raw" {
-#   url = "https://ipv6.icanhazip.com"
-# }
