e09dcecfca60d930c3e34392520784a5e34624b1
Zee Aslam <zeet6613@gmail.com>
2023-12-30 12:46:31 -0500

    (grafana-alerting) fix token

--- cluster/apps/flux-system/notifications/grafana/README.md
+++ cluster/apps/flux-system/notifications/grafana/README.md
@@ -4,4 +4,6 @@ This is making use of the [flux-grafana-provider](https://fluxcd.io/docs/compone
 
 I was working on a script to generate a token dynamically but ran out of time so I made one manually since I still have persistence enabled for grafana. I will not be able to make it ephemeral again until this is sorted.
 
-Since #4108 I am using keycloak for Grafana authentication. This is kaput until I adjust this workload to get credentials from keycloak and use that to authenticate with grafana. Should be much easier than dealing with grafana persistence. Can keep it ephemeral now
+Since #4108 I am using keycloak for Grafana authentication. This is kaput until I adjust this workload to get credentials from keycloak and use that to authenticate with grafana. Should be much easier than dealing with grafana persistence.
+
+TODO: Delete grafana-alerting-auth from repo once flow is properly established/tested
--- cluster/apps/flux-system/notifications/grafana/scripts/grafana-auth.sh
+++ cluster/apps/flux-system/notifications/grafana/scripts/grafana-auth.sh
@@ -2,11 +2,52 @@
 set -o nounset
 set -o errexit
 export NAMESPACE=flux-system
-export SECRET_NAME=grafana-auth-flux
+export IAM_SECRET_NAME=grafana-auth-flux
+export SECRET_NAME=grafana-alerting-auth
+
+if [[ -z ${XYZ_DOMAIN} ]]
+then
+  echo "XYZ_DOMAIN is not set!"
+  exit 1
+fi
+
+export IAM_TOKEN_ENDPOINT=https://iam.${XYZ_DOMAIN}/realms/bloopnet/protocol/openid-connect/token
+
+export IAM_CLIENT_ID=`kubectl -n ${NAMESPACE} get secret ${IAM_SECRET_NAME} --template={{.data.iam_client_id}} | base64 -d`
+export IAM_CLIENT_SECRET=`kubectl -n ${NAMESPACE} get secret ${IAM_SECRET_NAME} --template={{.data.iam_client_secret}} | base64 -d`
+
+function validateToken() {
+    token=$1
+    isactive = `curl -X POST \
+      -d "client_id=${IAM_CLIENT_ID}" \
+      -d "client_secret=${IAM_CLIENT_SECRET}" \
+      -d "token=$token" \
+      "${IAM_TOKEN_ENDPOINT}/introspect" | jq -r '.active'`
+    return $isactive
+}
+
+function renewToken() {
+    token=`curl -X POST \
+      -d "client_id=${IAM_CLIENT_ID}" \
+      -d "client_secret=${IAM_CLIENT_SECRET}" \
+      -d "grant_type=client_credentials" \
+      "${IAM_TOKEN_ENDPOINT}" | jq -r '.access_token'`
+    # TODO: Add to secret
+}
+
 # Check whether secret exists
 if [[ -n $(kubectl -n ${NAMESPACE} get secret ${SECRET_NAME} --ignore-not-found) ]]
 then
-    echo "Secret Exists. Not Requesting Neu Token"
+    echo "Secret Exists. Validating Token"
+    access_token=`kubectl -n ${NAMESPACE} get secret ${SECRET_NAME} --template={{.data.token}} | base64 -d`
+    isvalid=`validateToken $access_token`
+    if [[ isvalid ]]
+    then
+      echo "token is valid. Leaving it alone"
+      exit 0
+    else
+      renewToken
+    fi
 else
-    echo "No secret exists with name ${SECRET_NAME}"
-    # Add token acquisition logic here
+    echo "No secret exists with name ${SECRET_NAME}. Will generate one..."
+fi
